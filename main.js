a945aa8ba3b233a2f115aa4f5d1156075191f176
Eric Rovell <ericrovell@gmail.com>
2019-08-16 23:57:54 +0300

    text-align switcher, another colours for dark mode, theme switcher mobile layout fix

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ import getArticle from './app/fetchData.js';
 import render from './app/render.js';
 
 import allowThemeSwitch from './app/toggleTheme.js';
+import { articleAlign } from './app/articleAlign.js';
 
 async function renderArticle() {
   const container = document.querySelector('article');
   const article = await getArticle();
   render(article, container);
+  // render before align would be possible
+  articleAlign();
 }
 
 renderArticle();
-
 allowThemeSwitch();
